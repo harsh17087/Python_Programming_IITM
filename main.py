867c4250d6845c8bc6be827afb402ae52d28307c
Harsh Raj <harshraj17087@gmail.com>
2021-08-23 23:37:08 +0530

    main.py is updated

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("Hello World!")
-print("hello harsh, this is your first code")
+"""
+Welcome to my code repository of Python programs
+
+1.  This comprises of all 12 weeks of python tutorials which has been taught
+    by the IIT M BSc. in programming and Data science
+
+2.  IIT Madras Online degree program is an initiative by which IIT M will teach
+    maximum number of students without the barrier of IIT-JEE exams
+
+3.  This module is taught in the first year of this online BSc degree i.e. foundational degree
+
+4.  The course is taught by -
+
+    Sudarshan Iyengar,
+    Associate Professor,
+    Department of Computer Science & Engineering,
+    IIT Ropar
+
+
+"""
