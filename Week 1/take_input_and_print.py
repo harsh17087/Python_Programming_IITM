ced37ecaaa652a479c971b0d52b18caf274f9198
Harsh Raj <harshraj17087@gmail.com>
2021-08-23 23:50:33 +0530

    first program is added in week 1

---
+++ Week 1/take_input_and_print.py
@@ -0,0 +1,22 @@
+# We are gonna take a number as input and print it
+
+
+print("Enter a number:")
+n = int(input())
+print("The entered number is", n)
+n = n+1
+print(n)
+print(n+1)
+print(n+2)
+print(n+3)
+
+# The output of the above program is shown below for n=6
+'''
+Enter a number:
+6 
+The entered number is 6
+7
+8
+9
+10
+'''
