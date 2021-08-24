931ade7d926da0db3a62811ea150b7123b2d098a
Harsh Raj <harshraj17087@gmail.com>
2021-08-24 20:23:16 +0530

    More programs added in Week 1

--- Week 1/take_input_and_print.py
+++ Week 1/take_input_and_print.py
@@ -1,22 +1,40 @@
-# We are gonna take a number as input and print it
-
+# We are gonna take an integer number as input and print it
 
 print("Enter a number:")
 n = int(input())
 print("The entered number is", n)
-n = n+1
+n = n + 1
 print(n)
-print(n+1)
-print(n+2)
-print(n+3)
+print(n + 1)
+print(n + 2)
+print(n + 3)
 
-# The output of the above program is shown below for n=6
+# The output of the above program is shown below for n=9
 '''
 Enter a number:
-6 
-The entered number is 6
-7
-8
 9
+The entered number is 9
 10
+11
+12
+13
+
+'''
+
+# Another example to understand input and print function
+
+print("Enter your name, age and city in which you reside")
+name = str(input())
+age = int(input())
+city = str(input())
+print("My name is ", name , "and I am ", age ,"years old. I am from ", city)
+
+
+# The output of the above program is shown below
+'''
+Enter your name, age and city in which you reside
+harsh
+21
+Gaya
+My name is  harsh and I am  21 years old. I am from  Gaya
 '''
